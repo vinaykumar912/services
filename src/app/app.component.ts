968466b830c4567f0de9708f2152f0654e630c3a
vinay <vinay@gmail.com>
2021-03-03 19:40:32 +0530

    importing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CallingService} from './calling.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+   data:any;
+  constructor(private call:CallingService){
+  this.call.getdata().subscribe(data=>{
+    this.data=data
+    console.log(data)
+     })
+      };
 }
